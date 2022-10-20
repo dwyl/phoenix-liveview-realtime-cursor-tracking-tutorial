c40b7ef59a1b3d88ca1033ae614e1b34aa87a788
Luís Arteiro <lucho14garfield@gmail.com>
2022-10-20 14:16:37 +0100

    fix: Making LiveView template inside an html.heex file so intellisense works

--- lib/live_cursors_web/live/cursors.ex
+++ lib/live_cursors_web/live/cursors.ex
@@ -57,27 +57,4 @@ defmodule LiveCursorsWeb.Cursors do
 
     {:noreply, updated}
   end
-
-  def render(assigns) do
-    ~H"""
-      <ul class="list-none" id="cursor" phx-hook="TrackClientCursor">
-        <%= for user <- @users do %>
-          <li style={"color: deeppink; left: #{user.x}%; top: #{user.y}%"} class="flex flex-col absolute pointer-events-none whitespace-nowrap overflow-hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" width="31" height="32" fill="none" viewBox="0 0 31 32">
-                <path fill="url(#a)" d="m.609 10.86 5.234 15.488c1.793 5.306 8.344 7.175 12.666 3.612l9.497-7.826c4.424-3.646 3.69-10.625-1.396-13.27L11.88 1.2C5.488-2.124-1.697 4.033.609 10.859Z"/>
-                <defs>
-                  <linearGradient id="a" x1="-4.982" x2="23.447" y1="-8.607" y2="25.891" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#FF44F8"/>
-                    <stop offset="1" stop-color="#BDACFF"/>
-                  </linearGradient>
-                </defs>
-              </svg>
-            <span style={"background-color: deeppink;"} class="mt-1 ml-4 px-1 text-sm text-white">
-              <%= user.name %>
-            </span>
-          </li>
-        <% end %>
-      </ul>
-    """
-  end
 end
