fac41a7c3fe0a83d96c5e26415f1fd37788ee5d9
Luís Arteiro <lucho14garfield@gmail.com>
2022-10-20 11:52:32 +0100

    feat: Adding page with static custom cursor.

---
+++ lib/live_cursors_web/live/cursors.ex
@@ -0,0 +1,30 @@
+defmodule LiveCursorsWeb.Cursors do
+  use LiveCursorsWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    updated =
+      socket
+      |> assign(:mouse_x, 50)
+      |> assign(:mouse_y, 50)
+
+    {:ok, updated}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <ul class="list-none">
+      <li style={"color: deeppink; left: #{@mouse_x}%; top: #{@mouse_y}%"} class="flex flex-col absolute pointer-events-none whitespace-nowrap overflow-hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" width="31" height="32" fill="none" viewBox="0 0 31 32">
+      <path fill="url(#a)" d="m.609 10.86 5.234 15.488c1.793 5.306 8.344 7.175 12.666 3.612l9.497-7.826c4.424-3.646 3.69-10.625-1.396-13.27L11.88 1.2C5.488-2.124-1.697 4.033.609 10.859Z"/>
+      <defs>
+        <linearGradient id="a" x1="-4.982" x2="23.447" y1="-8.607" y2="25.891" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#FF44F8"/>
+          <stop offset="1" stop-color="#BDACFF"/>
+        </linearGradient>
+      </defs>
+    </svg>
+      </li>
+    </ul>
+    """
+  end
+end
--- lib/live_cursors_web/router.ex
+++ lib/live_cursors_web/router.ex
@@ -18,6 +18,7 @@ defmodule LiveCursorsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/cursors", Cursors
   end
 
   # Other scopes may use custom stacks.
