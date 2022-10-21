6162a61e5615c04cbec7f9f06fe26121faa929d0
Luís Arteiro <lucho14garfield@gmail.com>
2022-10-21 16:22:44 +0100

    fix: Fixing styles

--- test/live_cursors_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LiveCursorsWeb.PageControllerTest do
-  use LiveCursorsWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
