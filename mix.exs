869ab9545ff09bfe5b1f8ba7433e9c2ed2f86499
nelsonic <nelson+github@dwyl.com>
2024-01-05 16:57:41 +0000

    comment out error_tag/2 as not used #96

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule LiveCursors.MixProject do
     [
       {:phoenix, "~> 1.7.0"},
       {:phoenix_html, "~> 4.0"},
+      {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.0"},
       {:phoenix_view, "~> 2.0"},
